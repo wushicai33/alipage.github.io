f27e2a69bf90b1369bd33531c5b380dd2436d30b
wushicai33 <1209227630@qq.com>
2020-04-12 05:56:03 +0800

    aliPage

---
+++ precache-manifest.bb23ff536259c00e0fcfa4d9f022ba47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cbf9b8539d59f275dacd91da52c64e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "951c11f40781c497483b",
+    "url": "./static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "e07992c2f339e5657b4b",
+    "url": "./static/css/main.cf5e20b9.chunk.css"
+  },
+  {
+    "revision": "951c11f40781c497483b",
+    "url": "./static/js/2.43f4f9e4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.43f4f9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07992c2f339e5657b4b",
+    "url": "./static/js/main.558abf12.chunk.js"
+  },
+  {
+    "revision": "567ca5cea940a0c54188",
+    "url": "./static/js/runtime-main.1a632c21.js"
+  }
+]);
